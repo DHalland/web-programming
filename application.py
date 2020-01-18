5ae762d5f48ff3a1b3f56014e263596ab37b83ce
DHalland <dylanhalland@gmail.com>
2020-01-17 21:07:16 -0800

    messing around with postgres, sqlalchemy

--- application.py
+++ application.py
@@ -1,13 +1,35 @@
+import os
+
 from flask import Flask, render_template, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
+engine = create_engine(os.getenv("DATABASE_URL"))
+#db url --> C:\Program Files\PostgreSQL\12\bin
+db = scoped_session(sessionmaker(bind=engine))
+
 @app.route("/")
 def index():
-   return render_template("new.html")
+   flights = db.execute("SELECT * FROM flights").fetchall()
+   return render_template("index.html", flights=flights)
 
-@app.route("/user-info", methods=["POST"])
-def info():
-   email = request.form.get("user_email")
-   pwd = request.form.get("user_pwd")
-   return render_template("test.html", email=email, pwd=pwd)
+@app.route("/book", methods=["POST"])
+def book():
+    """Book a flight."""
+
+    # Get form information.
+    firstandlast = request.form.get("firstandlast")
+    try:
+        flight_id = int(request.form.get("flight_id"))
+    except ValueError:
+        return render_template("error.html", message="Invalid flight number.")
+
+    # Make sure the flight exists.
+    if db.execute("SELECT * FROM flights WHERE id = :id", {"id": flight_id}).rowcount == 0:
+        return render_template("error.html", message="No such flight with that id.")
+    db.execute("INSERT INTO passengers (name, flight_id) VALUES (:name, :flight_id)",
+            {"name": firstandlast, "flight_id": flight_id})
+    db.commit()
+    return render_template("success.html")
