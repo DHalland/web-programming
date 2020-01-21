092ba1ce34a81e4c2c9e27c406a31b30f5f3f7ad
DHalland <dylanhalland@gmail.com>
2020-01-20 19:08:21 -0800

    remote database config added

--- app.bat
+++ app.bat
@@ -1,3 +1,4 @@
+set DATABASE_URL=postgres://stuwkxxlkamgbx:0eb76a0c6de2da2e9fa5bf2f98c8e192377a5150e07a1b3f57c50202ed5e89f3@ec2-3-220-86-239.compute-1.amazonaws.com:5432/dbbq85n2g656qs
 set FLASK_APP=application.py
 set FLASK_ENV=development
 flask run
